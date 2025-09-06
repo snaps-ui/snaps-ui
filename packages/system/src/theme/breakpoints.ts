41450d898ca21c0db9ebdfa639254d6a3e0488b2
justice chimobi <chimobi.justice@gmail.com>
2025-09-06 09:46:07 +0100

    feat: snaps-ui toaster, move system themes to system package workspace

---
+++ packages/system/src/theme/breakpoints.ts
@@ -0,0 +1,28 @@
+// export const breakpointValues = {
+//   sm: 480,
+//   md: 768,
+//   lg: 1024,
+//   xl: 1280,
+//   '2xl': 1536,
+// };
+
+// export const breakpoints = Object.fromEntries(
+//   Object.entries(breakpointValues).map(([key, value]) => [key, `${value}px`])
+// );
+
+export const breakpoints = {
+  // Mobile devices (landscape)
+  sm: '480px',
+
+  // Tablets
+  md: '768px',
+
+  // Laptops and small desktops
+  lg: '1024px',
+
+  // Larger desktops
+  xl: '1280px',
+
+  // Extra-large monitors
+  '2xl': '1536px',
+}
