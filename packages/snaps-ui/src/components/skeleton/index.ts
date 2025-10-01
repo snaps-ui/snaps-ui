1051d96a2bdc60f0b9d056df212a83fd81f74ecc
justice chimobi <chimobi.justice@gmail.com>
2025-10-01 18:37:42 +0100

    fix: remove skeleton circle import

--- packages/snaps-ui/src/components/skeleton/index.ts
+++ packages/snaps-ui/src/components/skeleton/index.ts
@@ -1,5 +1,3 @@
 export { Skeleton, type SkeletonProps } from './skeleton'
 
-export { SkeletonCircle, SkeletonCircleProps } from './skeleton-circle'
-
 export { SkeletonText, type SkeletonTextProps } from './skeleton-text'
